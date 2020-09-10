0143a442b7543a2c7108573e77b3f1fdf2861cec
Sarvagya Bansal <sarvagyabansal@protonmail.com>
2020-09-10 12:44:48 +0530

    can delete todo items fixed bug cannot delete list due to foreign key error when items present in list

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,6 +5,16 @@ class TodoItemsController < ApplicationController
         @todo_item = @todo_list.todo_items.create(todo_item_params)
         redirect_to @todo_list
     end
+
+    def destroy
+        @todo_item = @todo_list.todo_items.find(params[:id])
+        if @todo_item.destroy
+         flash[:success] = "Todo List item was deleted."
+        else
+         flash[:error] = "Todo List item could not be deleted."
+        end
+        redirect_to @todo_list 
+    end
     
     private
     
